33bb456467e7e666e18eead0f83c337fe7b5ddb0
yaiwen <yaiwen96@gmail.com>
2022-04-19 00:56:50 +0800

    LeetCode Q26 RemoveDuplicatesFromSortedArray

---
+++ src/arraylist/Q26RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,25 @@
+package arraylist;
+
+public class Q26RemoveDuplicatesFromSortedArray {
+
+    // LeetCode 26. Remove Duplicates From Sorted Array
+    // Time Complexity: O(n) length of input nums
+    // Space Complexity: O(1)
+    public static void main(String[] args)  {
+        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] nums) {
+        int smallestIdx = 0;
+        int value = nums[0];
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i] > value) {
+                value = nums[i];
+                smallestIdx += 1;
+            }
+            nums[smallestIdx] = value;
+        }
+        return smallestIdx+1;
+    }
+}
