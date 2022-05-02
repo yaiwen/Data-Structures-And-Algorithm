61a0ea0e93a3665cc232d743034c37c92a94897a
yaiwen <yaiwen96@gmail.com>
2022-05-02 22:10:50 +0800

    Leetcode Q11

---
+++ src/leetcode/arraylist/medium/Q11ContainerWithMostWater.java
@@ -0,0 +1,35 @@
+package leetcode.arraylist.medium;
+
+import java.util.Arrays;
+
+public class Q11ContainerWithMostWater {
+
+    // LeetCode 11. Container With Most Water
+    // https://leetcode.com/problems/container-with-most-water/
+    // Time Complexity: O(n) loop through half of n
+    // Space Complexity: O(1)
+    public static void main(String[] args)  {
+        int[] nums = new int[]{1,8,6,2,5,4,8,3,7};
+        System.out.println(maxArea(nums));
+    }
+
+    public static int maxArea(int[] height) {
+        if (height.length == 2) {
+            return Math.min(height[0], height[1]);
+        }
+
+        int frontIdx = 0;
+        int backIdx = height.length - 1;
+        int maxArea = Math.min(height[frontIdx], height[backIdx]) * (backIdx - frontIdx);
+        while (frontIdx != backIdx) {
+            if (height[frontIdx] < height[backIdx]) {
+                frontIdx++;
+            } else {
+                backIdx--;
+            }
+            maxArea = Math.max(maxArea, (Math.min(height[frontIdx], height[backIdx]) * (backIdx - frontIdx)));
+        }
+
+        return maxArea;
+    }
+}
